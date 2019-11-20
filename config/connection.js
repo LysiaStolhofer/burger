a071dbf4e4e3e9111c6470840f00c982b615512f
Lysia Stolhofer <lysiapaulinstolhofer@gmail.com>
2019-11-19 22:35:06 -0500

    files modified

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,6 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection(require('./db-config'));
 
-
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
